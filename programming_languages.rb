6310f1014851d4fc5a81730dd6948ada0a225e97
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-23 19:33:40 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ require 'pry'
    languages.each do |style, description|
     description.each do |lang, type|
       if new_hash.has_key?(lang)
-        new_hash[lang][:style] << [style]
+        new_hash[lang][:style] << style
       else
         new_hash[lang] = type
         new_hash[lang][:style] = [style]
