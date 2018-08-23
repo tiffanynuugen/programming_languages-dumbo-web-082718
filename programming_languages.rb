873ef549da22b74596d58247c9da0f3d22d11d61
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-23 18:56:59 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
  def reformat_languages(languages)
+   new_hash = {}
    languages.each do |style, description|
     description.each do |lang, type|
       if new_hash.has_key?(lang)
