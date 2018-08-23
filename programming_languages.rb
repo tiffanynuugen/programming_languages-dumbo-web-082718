806ef351ecad71788ba7d1770069422a9de3f7d5
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-23 19:20:57 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,13 @@
 require 'pry'
-
  def reformat_languages(languages)
-  new_hash = {}
-   languages.each do |style, hash|
-    hash.each do |name, attribute|
-      if new_hash.has_key?(name)
-        new_hash[name][:style] << style
-        binding.pry
+  new_hash = {} 
+   languages.each do |style, description|
+    description.each do |lang, type|
+      if new_hash.has_key?(lang)
+        new_hash[lang][:style] << style
       else
-        new_hash[name] = type
-        new_hash[name][:style] = [style]
+        new_hash[lang] = type
+        new_hash[lang][:style] = [style]
       end
     end
   end
